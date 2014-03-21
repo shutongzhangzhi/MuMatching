9271e91dd8aade69cad2bc00d76ce28d4c6afb40
Zhangzhi <305080980@qq.com>
2014-03-21 18:19:32 +0800

    1.Create WM multi-pattern matching algorithm implementation.

---
+++ MuMatching/WM/Substring.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+using System.Diagnostics.Contracts;
+
+namespace MuMatching.WM {
+    /// <summary>
+    /// 代表某字符串的子串。
+    /// </summary>
+    internal struct Substring {
+
+        internal readonly string    Target;         // 目标字符串。 
+        internal readonly int       StartIndex;     // 子串位于目标串的开始位置
+        internal readonly int       Length;         // 子串长度
+
+        public Substring(string target, int startIndex, int length)
+            : this() {
+
+            Debug.Assert(target != null);
+            Debug.Assert(startIndex >= 0 && startIndex < target.Length);
+            Debug.Assert(length >= 0 && (startIndex + length) <= target.Length);
+
+            Target          = target;
+            StartIndex      = startIndex;
+            Length          = length;
+        }
+
+        #region Override Members
+
+        public override string ToString() { return Target.Substring(StartIndex, Length); }
+
+        #endregion
+    }
+}
+
---
+++ MuMatching/WM/WmInternalStateBuilder.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace MuMatching.WM
+{
+    internal sealed class WmInternalStateBuilder {
+        private const int DEFAULT_BLOCK_LENGTH = 2;
+        
+
+
+        public void AddPatterns(IEnumerable<string> patterns) {
+            Debug.Assert(patterns != null);
+
+            foreach (var pattern in patterns) {
+                if (!String.IsNullOrEmpty(pattern)) {
+                    AddPattern(pattern);
+                }
+            }
+        }
+
+        private void AddPattern(string pattern) {
+          
+        }
+
+
+        internal int MinPatternLength { get; set; }
+
+
+    }
+}
